842e687c72c7ad1c4081773bdb7c693ac860299d
CalviceGaming <diogo.pereira.d24@gmail.com>
2026-06-25 17:27:29 +0100

    player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,9 +7,17 @@ public class Player : MonoBehaviour, ISingleton
 {
     private int current_Money;
 
-    private Dictionary<int, ESlotColor> current_Bet;
-    
-    
+    public List<Item> Inventory {get; set;}
+
+    private ESlotColor[] numbers;
+
+    private Dictionary<int, int[]> bets;
+
+    private int[] multiplier_bets;
+
+    private int[] amount_bets;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,5 +28,25 @@ public class Player : MonoBehaviour, ISingleton
     void Update()
     {
         
+    }
+
+    void Confirm_Bet(int value)
+    {
+        for (int i = 0; i < bets.Count; i++)
+        {
+            for (int y = 0; i < bets[i].Length; i++)
+            {
+                if(bets[i][y] == value)
+                {
+                    Pay_Out(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    void Pay_Out(int bet_index)
+    {
+        current_Money += amount_bets[bet_index] * multiplier_bets[bet_index];
     }
 }
